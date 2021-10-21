456d5ce291843b033d6ba79b7d3c279bc03861d2
Jeremy Lee <>
2021-10-21 23:14:25 +0800

    added postgres

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,18 @@ const multer = require('multer')
 const upload = multer({dest: './temp/'})
 
 // NFT file upload setup
-// import { NFTStorage, File } from 'nft.storage'
 const nft_storage = require('nft.storage')
 const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDNFNzUwQTA0MkZENDBCN2QwZjYzNzIwODlGOTU1NUE5QzMxRTc3ZjkiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNDQ1OTUxNDYyMiwibmFtZSI6Im5mdGdhbl9zdGFnaW5nIn0.9B23zbY5szXHGWppHyUlr73nwms5qD7c_KMh3Ch33PU'
 const client = new nft_storage.NFTStorage({ token: apiKey })
+
+// Postgres connection
+const pg = require('pg')
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+})
 
 // RESTful setup
 app.use(express.urlencoded())
@@ -23,11 +31,26 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.get('/list', (req, res) => {
-    let response = {"header": "list", 
-                    "body": "list of all IDs"}
+app.get('/init', async (req, res) => {
+    
+    const db = await pool.connect()
+    await db.query("CREATE TABLE IF NOT EXISTS nftrecord (url text, created_on timestamp)")
+    
+    res.json({'result': 'ok'})
+    db.release()
+})
 
-    res.json(response)
+app.get('/nft', async (req, res) => {
+
+    try {
+        const db = await pool.connect()
+        const result = await db.query("SELECT * FROM nftrecord ORDER BY created_on DESC LIMIT 20")
+        const results = { 'results': (result) ? result.rows : null};
+        res.json(results)
+        db.release()
+    } catch(err) {
+        res.json({"status": "error", "message": err})
+    }
 })
 
 app.post('/nft', upload.single('nft_file'), (req, res) => {
@@ -45,14 +68,20 @@ app.post('/nft', upload.single('nft_file'), (req, res) => {
             image: new nft_storage.File([data], req.file.originalname, { type: req.file.mimetype })
         })
 
-        // console.log(metadata.embed())
-        // console.log(metadata.data.image)
-        console.log("https://ipfs.io/ipfs/" + metadata.data.image.hostname + metadata.data.image.pathname)
+        // construct NFT URL
+        nft_url = "https://ipfs.io/ipfs/" + metadata.data.image.hostname + metadata.data.image.pathname
 
+        // remove file
         fs.unlink(req.file.path, (err) => {
             if(err) console.error(err)
-        }) // remove file
-        res.json({'status':200, 'metadata': metadata})
+        }) 
+
+        // save to db
+        const db = await pool.connect()
+        await db.query("INSERT INTO nftrecord VALUES ($1, now())", [nft_url])
+        db.release()
+
+        res.json({'status':200, 'nft_url': nft_url})
     })
     
 })
--- backend/readme.md
+++ backend/readme.md
@@ -4,6 +4,15 @@
 
 ## Run on localhost
 > heroku local web
+# for first time execution, create the database first
+> localhost:5000/init
+
+## Create and run remote server and database
+# Server
+> heroku ps:scale web=1
+> heroku open
+# Database
+> heroku addons:create heroku-postgresql:hobby-dev
 
 ## Run on Heroku server
 # NOTE: Server resided on a sub folder. To push code use the following command on the root directory
@@ -11,3 +20,11 @@
 > git subtree push --prefix backend heroku master
 > heroku ps:scale web=1
 > heroku open
+
+## DB record
+# Create
+> create table nftrecord (url text, created_on timestamp);
+# insert 1 record
+> insert into nftrecord values ('https://ipfs.io/ipfs/bafybeifkkn4ddunvmxvc6llearqx3pehz55ightxr7cc4bim62j7w6txjm/result.jpeg', NOW());
+# select statement
+> select * from nftrecord order by created_on desc;
